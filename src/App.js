161ab239dd341f35e3469c019310913a93234ea1
Kevin <kevincarr632@gmail.com>
2020-03-10 20:56:57 -0500

    Added placeholder to display on jobsave success. Also added image icon to manifest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Swal from 'sweetalert2'
 
 function App() {
+
+  useEffect(() => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: false,
+      onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Job Saved Successfully !'
+    })
+  })    
   return (
     <div className="App">
       
