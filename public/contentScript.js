8683ce667955c156ffc50b0414b308eb0620863f
Kevin <kevincarr632@gmail.com>
2020-03-25 22:26:00 -0500

    added redirect to login if jwt is expired

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,11 +1,10 @@
 chrome.runtime.onMessage.addListener(request => {
   if (request.type === "getUrl") {
     chrome.storage.local.get('token', function (result) {
-      const accessToken = result.token
-
       if (result.token === undefined) {
         return chrome.runtime.sendMessage({ type: "getToken" })
       }
+      const accessToken = result.token
       const data = { jobTitle: request.title, url: request.url };
 
       fetch('http://localhost:8080/users/addJob', {
@@ -21,7 +20,7 @@ chrome.runtime.onMessage.addListener(request => {
         })
         .then((data) => {
           if (data.message === "Job Post Created") {
-            
+
             const modal = document.createElement('iframe');
             modal.setAttribute("style", "border: none; display: block; height: 60%; width: 200px; overflow: hidden; position: fixed; right: 0px; top: 0px; left: auto; float: none; width: auto; z-index: 2147483647; background: transparent;")
             modal.id = "jobSave"
@@ -31,6 +30,10 @@ chrome.runtime.onMessage.addListener(request => {
             iframe.src = chrome.runtime.getURL('./index.html')
             iframe.frameBorder = 0;
           }
+
+          if (data === "Jwt is expired") {
+            return chrome.runtime.sendMessage({ type: "getToken" })
+          }
         })
         .catch((error) => {
           console.error('Error:', error);
@@ -39,20 +42,17 @@ chrome.runtime.onMessage.addListener(request => {
   }
 
   if (request.type === "getTokenFromStorage") {
+    // need to add our actual deployed link here
     if (window.location.href === "http://localhost:3000/dashboard") {
       return setToken()
     }
-  }
-
-  if (request.type === "sign-out") {
-
   }
 })
 
 
 const setToken = () => {
   const token = localStorage.getItem('token')
-  chrome.storage.local.set({ token });
+  return chrome.storage.local.set({ token })
 }
 
 
