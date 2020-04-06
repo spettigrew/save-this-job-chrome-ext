5162216dc5b56b58ebc66b0d2a5f729315dad77d
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-04-06 18:43:23 -0500

    replace localhost with staging urls

--- extension/background.js
+++ extension/background.js
@@ -1,153 +1,158 @@
-
 // This is where we create the context menu that's available through left click in the browser
-chrome.storage.local.get('token', result => {
+chrome.storage.local.get('token', (result) => {
   if (result.token) {
     chrome.contextMenus.create({
-      id: "First",
-      title: "Add Job Post",
+      id: 'First',
+      title: 'Add Job Post',
       contexts: ['all'],
-      visible: true
-    })
+      visible: true,
+    });
     chrome.contextMenus.create({
-      id: "Fourth",
-      title: "View Dashboard",
+      id: 'Fourth',
+      title: 'View Dashboard',
       contexts: ['all'],
-      visible: true
-    })
+      visible: true,
+    });
     chrome.contextMenus.create({
-      id: "Second",
-      title: "Logout",
+      id: 'Second',
+      title: 'Logout',
       contexts: ['all'],
-      visible: true
-    })
+      visible: true,
+    });
     chrome.contextMenus.create({
-      id: "Third",
-      title: "Login",
+      id: 'Third',
+      title: 'Login',
       contexts: ['all'],
-      visible: false
-    })
-   
+      visible: false,
+    });
   } else {
     chrome.contextMenus.create({
-      id: "Third",
-      title: "Login",
+      id: 'Third',
+      title: 'Login',
       contexts: ['all'],
-      visible: true
-    })
+      visible: true,
+    });
     chrome.contextMenus.create({
-      id: "Second",
-      title: "Logout",
+      id: 'Second',
+      title: 'Logout',
       contexts: ['all'],
-      visible: false
-    })
+      visible: false,
+    });
     chrome.contextMenus.create({
-      id: "Fourth",
-      title: "View Dashboard",
+      id: 'Fourth',
+      title: 'View Dashboard',
       contexts: ['all'],
-      visible: false
-    })
+      visible: false,
+    });
     chrome.contextMenus.create({
-      id: "First",
-      title: "Add Job Post",
+      id: 'First',
+      title: 'Add Job Post',
       contexts: ['all'],
-      visible: false
-    })
+      visible: false,
+    });
   }
-})
-
+});
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   // This is where we add a event listener for when a user clicks on our context menu items we created above and sends a message to the current tab with the url and title
   if (tab) {
-    if (info.menuItemId === "First") {
+    if (info.menuItemId === 'First') {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         const tabTitle = tabs[0].title;
         const tabUrl = tabs[0].url;
         chrome.tabs.sendMessage(tabs[0].id, {
-          type: "getUrl",
+          type: 'getUrl',
           title: tabTitle,
-          url: tabUrl
-        })
-      })
+          url: tabUrl,
+        });
+      });
     }
 
-    if (info.menuItemId === "Second") {
-      chrome.storage.local.get('token', result => {
+    if (info.menuItemId === 'Second') {
+      chrome.storage.local.get('token', (result) => {
         if (result.token) {
-          return signOut()
+          return signOut();
         } else {
-          return chrome.contextMenus.update('Second', { visible: false })
+          return chrome.contextMenus.update('Second', { visible: false });
         }
-      })
+      });
     }
 
-    if (info.menuItemId === "Third") {
-      chrome.storage.local.get('token', result => {
+    if (info.menuItemId === 'Third') {
+      chrome.storage.local.get('token', (result) => {
         if (!result.token) {
-          return login()
+          return login();
         } else {
-          return chrome.contextMenus.update('Third', { visible: true })
+          return chrome.contextMenus.update('Third', { visible: true });
         }
-      })
+      });
     }
 
-    if (info.menuItemId === "Fourth") {
-      chrome.storage.local.get('token', result => {
+    if (info.menuItemId === 'Fourth') {
+      chrome.storage.local.get('token', (result) => {
         if (result.token) {
-          return chrome.tabs.create({ 'url': 'http://localhost:3000/dashboard' })
+          return chrome.tabs.create({
+            url: 'https://staging.d3d1q8nq7a3fmz.amplifyapp.com/dashboard',
+          });
         } else {
-          return chrome.contextMenus.update('Fourth', { visible: true })
+          return chrome.contextMenus.update('Fourth', { visible: true });
         }
-      })
+      });
     }
   }
-})
+});
 
-chrome.runtime.onMessage.addListener(request => {
-  if (request.type === "getToken") {
-    return login()
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.type === 'getToken') {
+    return login();
   }
 
-  if (request.type === "jobSaveSuccess") {
+  if (request.type === 'jobSaveSuccess') {
     const notificationOptions = {
-      type: "basic",
-      iconUrl: "./images/icon48.png",
-      title: "Job Save Success!",
-      message: "Your Job url was successfully saved to your dashboard!"
-    }
-    return chrome.notifications.create(notificationOptions)
+      type: 'basic',
+      iconUrl: './images/icon48.png',
+      title: 'Job Save Success!',
+      message: 'Your Job url was successfully saved to your dashboard!',
+    };
+    return chrome.notifications.create(notificationOptions);
   }
 
-  if (request.type === "Error") {
+  if (request.type === 'Error') {
     const notificationOptions = {
-      type: "basic",
-      iconUrl: "./images/icon48.png",
-      title: "Job Save Error",
-      message: "There was a problem saving your job post to the database. Please try again later."
-    }
-    return chrome.notifications.create(notificationOptions)
+      type: 'basic',
+      iconUrl: './images/icon48.png',
+      title: 'Job Save Error',
+      message:
+        'There was a problem saving your job post to the database. Please try again later.',
+    };
+    return chrome.notifications.create(notificationOptions);
   }
 
-  if (request.type === "tokenSet") {
+  if (request.type === 'tokenSet') {
     chrome.contextMenus.update('Second', { visible: true }, function () {
       chrome.contextMenus.update('Third', { visible: false }, function () {
         chrome.contextMenus.update('Fourth', { visible: true }, function () {
-          chrome.contextMenus.update('First', { visible: true })
-        })
-      })
-    })
+          chrome.contextMenus.update('First', { visible: true });
+        });
+      });
+    });
   }
-})
+});
 
 function login() {
-  chrome.tabs.create({ 'url': 'http://localhost:3000/login' }, function () {
-    chrome.tabs.onUpdated.addListener(() => {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        const tabId = tabs[0].id
-        chrome.tabs.sendMessage(tabId, { type: "getTokenFromStorage" })
-      })
-    })
-  })
+  chrome.tabs.create(
+    { url: 'https://staging.d3d1q8nq7a3fmz.amplifyapp.com/login' },
+    function () {
+      chrome.tabs.onUpdated.addListener(() => {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (
+          tabs,
+        ) {
+          const tabId = tabs[0].id;
+          chrome.tabs.sendMessage(tabId, { type: 'getTokenFromStorage' });
+        });
+      });
+    },
+  );
 }
 
 function signOut() {
@@ -157,19 +162,16 @@ function signOut() {
         chrome.contextMenus.update('Fourth', { visible: false }, function () {
           chrome.contextMenus.update('First', { visible: false }, function () {
             const notificationOptions = {
-              type: "basic",
-              iconUrl: "./images/icon48.png",
-              title: "Sign-Out Success!",
-              message: "Your are now logged off. Try saving a new job to log back in!"
-            }
-            return chrome.notifications.create(notificationOptions)
-          })
-        })
-      })
-    })
-  })
+              type: 'basic',
+              iconUrl: './images/icon48.png',
+              title: 'Sign-Out Success!',
+              message:
+                'Your are now logged off. Try saving a new job to log back in!',
+            };
+            return chrome.notifications.create(notificationOptions);
+          });
+        });
+      });
+    });
+  });
 }
-
-
-
-
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,15 +1,25 @@
-chrome.runtime.onMessage.addListener(request => {
-  if (request.type === "getUrl") {
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.type === 'getUrl') {
     chrome.storage.local.get('token', function (result) {
       if (result.token === undefined) {
-        return chrome.runtime.sendMessage({ type: "getToken" })
+        return chrome.runtime.sendMessage({ type: 'getToken' });
       }
 
-      const accessToken = result.token
-      const logo = document.querySelector('#vjs-img-cmL') || document.querySelector('.vjs-JobInfoHeader-logo-container img') || document.querySelector('.jobsearch-CompanyAvatar-image') || null
-      const title = document.querySelector('#vjs-jobtitle') || document.querySelector('.jobsearch-JobInfoHeader-title-container h3')
-      const company = document.querySelector('#vjs-cn a') || document.querySelector('#vjs-cn') || document.querySelector('.icl-u-lg-mr--sm a') || document.querySelector('.icl-u-lg-mr--sm')
-      setCompanyName(company)
+      const accessToken = result.token;
+      const logo =
+        document.querySelector('#vjs-img-cmL') ||
+        document.querySelector('.vjs-JobInfoHeader-logo-container img') ||
+        document.querySelector('.jobsearch-CompanyAvatar-image') ||
+        null;
+      const title =
+        document.querySelector('#vjs-jobtitle') ||
+        document.querySelector('.jobsearch-JobInfoHeader-title-container h3');
+      const company =
+        document.querySelector('#vjs-cn a') ||
+        document.querySelector('#vjs-cn') ||
+        document.querySelector('.icl-u-lg-mr--sm a') ||
+        document.querySelector('.icl-u-lg-mr--sm');
+      setCompanyName(company);
 
       const data = {
         jobTitle: title.textContent,
@@ -17,60 +27,58 @@ chrome.runtime.onMessage.addListener(request => {
         logo: logo ? logo.src : null,
         companyTitle: company.info ? company.info.company : company.name,
         companyUrl: company.info ? company.info.companyUrl : null,
-      }
+      };
 
       fetch('http://localhost:8080/users/addJob', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${accessToken}`
+          Authorization: `Bearer ${accessToken}`,
         },
         body: JSON.stringify(data),
       })
         .then((response) => {
-          return response.json()
+          return response.json();
         })
         .then((data) => {
           if (data === 'Jwt is expired') {
-            return chrome.runtime.sendMessage({ type: "getToken" })
+            return chrome.runtime.sendMessage({ type: 'getToken' });
           }
-          if (data.message === "Job Post Created") {
-            return chrome.runtime.sendMessage({ type: "jobSaveSuccess" })
+          if (data.message === 'Job Post Created') {
+            return chrome.runtime.sendMessage({ type: 'jobSaveSuccess' });
           }
         })
         .catch((error) => {
           console.error('Error:', error);
-          chrome.runtime.sendMessage({ type: "Error" })
+          chrome.runtime.sendMessage({ type: 'Error' });
         });
     });
   }
 
-  if (request.type === "getTokenFromStorage") {
+  if (request.type === 'getTokenFromStorage') {
     // need to add our actual deployed link here
-    if (window.location.href === "http://localhost:3000/dashboard") {
-      return setToken()
+    if (
+      window.location.href ===
+      'https://staging.d3d1q8nq7a3fmz.amplifyapp.com/dashboard'
+    ) {
+      return setToken();
     }
   }
-})
-
+});
 
 const setToken = () => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem('token');
   chrome.storage.local.set({ token }, () => {
-    chrome.runtime.sendMessage({ type: "tokenSet" })
-  })
-}
+    chrome.runtime.sendMessage({ type: 'tokenSet' });
+  });
+};
 
 const setCompanyName = (company) => {
   if (company.href) {
-    company.info = { company: company.textContent, companyUrl: company.href }
-    return company.info
+    company.info = { company: company.textContent, companyUrl: company.href };
+    return company.info;
   } else {
-    company.name = company.textContent
-    return company.name
+    company.name = company.textContent;
+    return company.name;
   }
-}
-
-
-
-
+};
