9fba7e74919fc1e5aad6bcfa5b9fcfe70b44baa6
Kevin <kevincarr632@gmail.com>
2020-04-22 16:17:33 -0500

    updated popup.js to remove on start and show on login

--- extenstion/background.js
+++ extenstion/background.js
@@ -1,126 +1,124 @@
-chrome.runtime.onInstalled.addListener(function () {
+
+chrome.contextMenus.create({
+  id: 'show',
+  title: 'Show',
+  contexts: ['all'],
+  visible: false,
+});
+
+chrome.contextMenus.create({
+  id: 'hide',
+  title: 'Hide',
+  contexts: ['all'],
+  visible: true,
+});
+
+chrome.storage.local.get('token', (result) => {
+  if (result.token) {
     chrome.contextMenus.create({
-      id: 'show',
-      title: 'Show',
+      id: 'ViewDashboard',
+      title: 'View Dashboard',
+      contexts: ['all'],
+      visible: true,
+    });
+    chrome.contextMenus.create({
+      id: 'logout',
+      title: 'Logout',
+      contexts: ['all'],
+      visible: true,
+    });
+    chrome.contextMenus.create({
+      id: 'login',
+      title: 'Login',
       contexts: ['all'],
       visible: false,
     });
 
+  } else {
     chrome.contextMenus.create({
-      id: 'hide',
-      title: 'Hide',
+      id: 'login',
+      title: 'Login',
       contexts: ['all'],
       visible: true,
     });
-
-  chrome.storage.local.get('token', (result) => {
-    if (result.token) {
-      chrome.contextMenus.create({
-        id: 'ViewDashboard',
-        title: 'View Dashboard',
-        contexts: ['all'],
-        visible: true,
-      });
-      chrome.contextMenus.create({
-        id: 'logout',
-        title: 'Logout',
-        contexts: ['all'],
-        visible: true,
-      });
-      chrome.contextMenus.create({
-        id: 'login',
-        title: 'Login',
-        contexts: ['all'],
-        visible: false,
-      });
-
-    } else {
-      chrome.contextMenus.create({
-        id: 'login',
-        title: 'Login',
-        contexts: ['all'],
-        visible: true,
-      });
-      chrome.contextMenus.create({
-        id: 'logout',
-        title: 'Logout',
-        contexts: ['all'],
-        visible: false,
-      });
-      chrome.contextMenus.create({
-        id: 'ViewDashboard',
-        title: 'View Dashboard',
-        contexts: ['all'],
-        visible: false,
-      });
-    }
-  });
-})
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  // This is where we add a event listener for when a user clicks on our context menu items we created above and sends a message to the current tab with the url and title
-  if (tab) {
-
-    if (info.menuItemId === 'hide') {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (
-        tabs,
-      ) {
-        const tabId = tabs[0].id;
-        chrome.tabs.sendMessage(tabId, { type: 'hide' }, () => {
-          chrome.contextMenus.update('hide', { visible: false }, () => {
-            chrome.contextMenus.update('show', { visible: true })
-          })
-        });
-      });
-    }
-
-    if (info.menuItemId === 'show') {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (
-        tabs,
-      ) {
-        const tabId = tabs[0].id;
-        chrome.tabs.sendMessage(tabId, { type: 'show' }, () => {
-          chrome.contextMenus.update('show', { visible: false }, () => {
-            chrome.contextMenus.update('hide', { visible: true })
-          })
-        });
-      });
-    }
-
-    if (info.menuItemId === 'logout') {
-      chrome.storage.local.get('token', (result) => {
-        if (result.token) {
-          return signOut();
-        } else {
-          return chrome.contextMenus.update('logout', { visible: false });
-        }
-      });
-    }
-
-    if (info.menuItemId === 'login') {
-      chrome.storage.local.get('token', (result) => {
-        if (!result.token) {
-          return login();
-        } else {
-          return chrome.contextMenus.update('login', { visible: true });
-        }
-      });
-    }
-
-    if (info.menuItemId === 'ViewDashboard') {
-      chrome.storage.local.get('token', (result) => {
-        if (result.token) {
-          return chrome.tabs.create({
-            url: 'https://staging.d3d1q8nq7a3fmz.amplifyapp.com/dashboard',
-          });
-        } else {
-          return chrome.contextMenus.update('ViewDashboard', { visible: true });
-        }
-      });
-    }
+    chrome.contextMenus.create({
+      id: 'logout',
+      title: 'Logout',
+      contexts: ['all'],
+      visible: false,
+    });
+    chrome.contextMenus.create({
+      id: 'ViewDashboard',
+      title: 'View Dashboard',
+      contexts: ['all'],
+      visible: false,
+    });
   }
 });
 
+chrome.contextMenus.onClicked.addListener((info) => {
+
+  if (info.menuItemId === 'hide') {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (
+      tabs,
+    ) {
+      const tabId = tabs[0].id;
+      chrome.tabs.sendMessage(tabId, { type: 'hide' }, () => {
+        chrome.contextMenus.update('hide', { visible: false }, () => {
+          chrome.contextMenus.update('show', { visible: true })
+        })
+      });
+    });
+  }
+
+  if (info.menuItemId === 'show') {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (
+      tabs,
+    ) {
+      const tabId = tabs[0].id;
+      chrome.tabs.sendMessage(tabId, { type: 'show' }, () => {
+        chrome.contextMenus.update('show', { visible: false }, () => {
+          chrome.contextMenus.update('hide', { visible: true })
+        })
+      });
+    });
+  }
+
+  if (info.menuItemId === 'logout') {
+    chrome.storage.local.get('token', (result) => {
+      if (result.token) {
+        return signOut();
+      } else {
+        return chrome.contextMenus.update('logout', { visible: false });
+      }
+    });
+  }
+
+  if (info.menuItemId === 'login') {
+    chrome.storage.local.get('token', (result) => {
+      if (!result.token) {
+        return login();
+      } else {
+        return chrome.contextMenus.update('login', { visible: true });
+      }
+    });
+  }
+
+  if (info.menuItemId === 'ViewDashboard') {
+    chrome.storage.local.get('token', (result) => {
+      if (result.token) {
+        return chrome.tabs.create({
+          url: 'https://staging.d3d1q8nq7a3fmz.amplifyapp.com/dashboard',
+        });
+      } else {
+        return chrome.contextMenus.update('ViewDashboard', { visible: true });
+      }
+    });
+  }
+
+});
+
+let timeout
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'getToken') {
     return login();
@@ -150,18 +148,35 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'tokenSet') {
     chrome.contextMenus.update('logout', { visible: true }, function () {
       chrome.contextMenus.update('login', { visible: false }, function () {
-        chrome.contextMenus.update('ViewDashboard', { visible: true }, function() {
-          setTimeout(() => {
-            chrome.contextMenus.update('logout', {visible: false }, function () {
-              chrome.contextMenus.update('login', {visible: true }, function () {
-                chrome.contextMenus.update('ViewDashboard', {visible: false });
-              })
+        chrome.contextMenus.update('ViewDashboard', { visible: true }, function () {
+          chrome.tabs.onActivated.addListener(function (activeInfo) {
+            chrome.tabs.onUpdated.addListener(function () {
+              chrome.tabs.sendMessage(activeInfo.tabId, { type: 'show' })
+              runTokenTimer()
             })
-          }, 3000000)
-        } );
+          })
+        });
       });
     });
   }
+
+  function runTokenTimer() {
+    timeout = setTimeout(() => {
+      chrome.contextMenus.update('logout', { visible: false }, function () {
+        chrome.contextMenus.update('login', { visible: true }, function () {
+          chrome.contextMenus.update('ViewDashboard', { visible: false }, () => {
+            chrome.storage.local.remove('token')
+            chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, { type: "hide" })
+            })
+          });
+        })
+      })
+    }, 3000000)
+
+    return timeout
+  }
+
 });
 
 function login() {
@@ -181,21 +196,35 @@ function login() {
 }
 
 function signOut() {
-  chrome.storage.local.clear(function () {
+  chrome.storage.local.remove('token', function () {
+    chrome.runtime.lastError
     chrome.contextMenus.update('login', { visible: true }, function () {
       chrome.contextMenus.update('logout', { visible: false }, function () {
         chrome.contextMenus.update('ViewDashboard', { visible: false }, function () {
-          const notificationOptions = {
-            type: 'basic',
-            iconUrl: './images/icon48.png',
-            title: 'Sign-Out Success!',
-            message:
-              'Your are now logged off. Try saving a new job to log back in!',
-          };
-          return chrome.notifications.create(notificationOptions);
+          chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { type: "hide" })
+            clearTimeout(timeout)
+            const notificationOptions = {
+              type: 'basic',
+              iconUrl: './images/icon48.png',
+              title: 'Sign-Out Success!',
+              message:
+                'Your are now logged off. Try saving a new job to log back in!',
+            };
+            return chrome.notifications.create(notificationOptions);
+          })
         })
       });
     });
   });
 }
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  console.log('activated')
+  chrome.runtime.lastError
+  chrome.tabs.sendMessage(activeInfo.tabId, { type: "tabActivated" }, () => {
+    chrome.runtime.lastError
+    console.log('tabActivated message sent')
+  })
+})
 
